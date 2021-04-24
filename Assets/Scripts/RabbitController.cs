ec847d607f94c229963c46433262fba7a153dc8e
Jc-42 <83050748+Jc-42@users.noreply.github.com>
2021-04-24 14:44:09 -0500

    Added Wander A.I. For Rabbits
    Rabbit will now move to a random position and wait for x seconds then move again.

--- Assets/Scripts/RabbitController.cs
+++ Assets/Scripts/RabbitController.cs
@@ -10,36 +10,54 @@ public class RabbitController : MonoBehaviour
     public int rabbitViewDistance = 20;
     public NavMeshAgent agent;
 
+    public float time = 0;
+
     float xPos;
     float yPos;
     float zPos;
     float rabbitXPos;
     float rabbitZPos;
-    
+
+    int randomTime;
+
     Vector3 movePos;
     void Start()
     {
-        
+        // create a random time that the creature will wait for
+        randomTime = Random.Range(3, 8);
 
-        
+        // run the 'timer' meathod every second
+        InvokeRepeating("timer", 1f , 1f );
 
-       
+        // make creature move to random position
+        RandomPosition();
     }
 
     
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
+        
 
+        if (time >= randomTime)
+        {
+            // every frame chack if the time the creature has waited has surpassed the random max wait time if so reset the random time and the wait time then..
+            randomTime = Random.Range(3, 8);
+            time = 0;
+
+
+            //..calculate a random position and move the creature to it
             RandomPosition();
+            agent.SetDestination(movePos);
 
         }
+
+      
+       
     }
 
      public void RandomPosition() 
      {   
-         // get current rabbit position
-
+         // get current creature position
          rabbitXPos = transform.position.x;
          rabbitZPos = transform.position.z;
 
@@ -48,32 +66,38 @@ public class RabbitController : MonoBehaviour
          xPos = Random.Range((rabbitXPos - rabbitViewDistance), (rabbitXPos + rabbitViewDistance));
          zPos = Random.Range((rabbitZPos - rabbitViewDistance), (rabbitZPos + rabbitViewDistance));
 
-         // move rabbit to the random location
+         // set movPos to that location
          movePos = new Vector3(xPos, 0, zPos);
 
+        // change the y value of the random location to the hight of the ground at that random point
          movePos.y = Terrain.activeTerrain.SampleHeight(movePos);
 
-         Debug.Log("x " + rabbitXPos + " z " + rabbitZPos + " xPos " + xPos + " zPos " + zPos + " movePos " + movePos);
+    }
 
-         agent.SetDestination(movePos); }
+    public void timer()
+    {
 
-    /*public static Vector3 RandomNavSphere (Vector3 origin, float distance, int layermask) {
-
-        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * distance;
-
-        randomDirection += origin;
-
-        NavMeshHit navHit;
-
-        NavMesh.SamplePosition(randomDirection, out navHit, distance, layermask);
-        
-        test = navHit.position;
-        
-        return navHit.position;
-
-        
-    }*/
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    // if the creature has stopped moving then advance the time it has waited
+                    time = time + 1f;
+                    
 
 
+
+
+
+
+
+                }
+            }
+        }
+       
+
+    }
 
 }
